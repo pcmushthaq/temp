5f089eabd3340c090b7de427d1a01404edf96300
Mushthaq <37449256+pcmushthaq@users.noreply.github.com>
2022-05-30 02:07:38 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -7,30 +7,26 @@
 
 void main()
 {
-    printf("Client Program\n");
-    char tcpbuffer[50];
+    printf("Client Code Running..\n");
+    char tcp_buffer[50];
     int sockfd;
-
     // Create client socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_address;
-
+    struct sockaddr_in addr1;
     // Assign IP,PORT
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = 5000;
-
+    addr1.sin_family = AF_INET;
+    addr1.sin_addr.s_addr = INADDR_ANY;
+    addr1.sin_port = 4444;
     // Connect cient socket to server socket
-    connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
-    printf("Client is ready to send messages.\n");
+    connect(sockfd, (struct sockaddr *)&addr1, sizeof(addr1));
     do
     {
         printf("Enter your message:\n");
-        gets(tcpbuffer);
-        printf("Waiting for reply from server\n");
-        send(sockfd, tcpbuffer, sizeof(tcpbuffer), 0);
-        recv(sockfd, tcpbuffer, sizeof(tcpbuffer), 0);
-        printf("Recieved from server:%s\n", tcpbuffer);
+        gets(tcp_buffer);
+        printf("Waiting for reply from server...\n");
+        send(sockfd, tcp_buffer, sizeof(tcp_buffer), 0);
+        recv(sockfd, tcp_buffer, sizeof(tcp_buffer), 0);
+        printf("Recieved from server: %s\n", tcp_buffer);
 
-    } while (strcmp(tcpbuffer, "exit") != 0);
+    } while (strcmp(tcp_buffer, "exit") != 0);
 }
--- server.c
+++ server.c
@@ -7,36 +7,35 @@
 
 void main()
 {
-    printf("Server Program\n");
-    char tcpbuffer[50];
+    printf("Server Code Running..\n");
+    char tcp_buffer[50];
     int sockfd, connfd;
     // Create server scoket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_address, cli;
+    struct sockaddr_in addr1, addr2;
     /// Assign IP and PORT
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = 5000;
+    addr1.sin_family = AF_INET;
+    addr1.sin_addr.s_addr = INADDR_ANY;
+    addr1.sin_port = 4444;
     int s = sizeof(struct sockaddr_in);
     // Bind newly created socket to given IP
-    bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    bind(sockfd, (struct sockaddr *)&addr1, sizeof(addr1));
     // Now server is ready to listen
     listen(sockfd, 5);
     // accept the data packet from client
-    connfd = accept(sockfd, (struct sockaddr *)&cli, (&s));
-
-    // Code for continuing chat
+    connfd = accept(sockfd, (struct sockaddr *)&addr2, (&s));
+    printf("Chat with the client. Type exit to close the chat.");
     do
     {
-        printf("\nWaiting for response from client\n");
-        recv(connfd, tcpbuffer, sizeof(tcpbuffer), 0);
-        printf("Received message from client: %s\n", tcpbuffer);
-        printf("Enter your message:Type exit to close chat. \n");
-        gets(tcpbuffer);
-        send(connfd, tcpbuffer, sizeof(tcpbuffer), 0);
+        printf("Waiting for response from client..\n");
+        recv(connfd, tcp_buffer, sizeof(tcp_buffer), 0);
+        printf("Received message from client: %s\n", tcp_buffer);
+        printf("Enter your message: \n");
+        gets(tcp_buffer);
+        send(connfd, tcp_buffer, sizeof(tcp_buffer), 0);
     } 
     // The loop will continue until an exit message is typed from the server side
-    while (strcmp(tcpbuffer, "exit") != 0);
+    while (strcmp(tcp_buffer, "exit") != 0);
 
     close(connfd);
     close(sockfd);
